70cc5f96490cf9b6ceeb9cab962d1cc0a8588333
Jasper den Ouden <o.jasper@gmail.com>
2012-11-11 17:08:51 +0100

    hmm messy

--- parse/mess/treekenize.jl
+++ parse/mess/treekenize.jl
@@ -21,21 +21,23 @@ end
 
 test2("a,b,c left d e f,g rightrightleftleftrorightrightskaleftkitten")
 
-load("c_treekenize.jl")
+load("c_parse.jl")
 
-testc(str) = encode(stream_from_string(str))
+testc(str) = c_parse_top(stream_from_string(str))
 
-#testc("sqr(x,y+z,q)")
-
+##testc("sqr(x,y+z,q)")
+#
 function testc(n::Integer)
     list = {}
-    @with s = open("test/SDL.h.e") for i=1:n
-        push(list, encode(s))
+    @with s = open("test/gl.h.e") for i=1:n
+        push(list, c_parse_top(s))
     end
     return list
 end
 
+println(testc("void* function_with(Type* pointers);")) 
 println(testc("extern __attribute__ ((visibility(\"default\"))) Uint8 SDL_GetAppState(void);"))
+
 
 println(testc("extern unsigned long long int gnu_dev_makedev (unsigned int __major,
             unsigned int __minor)
